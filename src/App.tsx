0b686c57b7f8e0731fd8c7e803ffcfe4c70360e2
Hızır Kocaman <hizirrkocaman@gmail.com>
2026-05-03 01:43:19 +0300

    setup donee

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import { usePokemons } from "./hooks/usePokemons";
-
 const App = () => {
-  const { data: pokemons } = usePokemons();
-  return (
-    <ul>
-      {pokemons?.map((pokemon) => (
-        <li key={pokemon.id}>{pokemon.name}</li>
-      ))}
-    </ul>
-  );
+  return <div>App</div>;
 };
 
 export default App;
